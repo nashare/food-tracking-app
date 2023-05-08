fd18b00f6da02093c22b2b4db58e6041c34da16f
Daniel JS <daniel@mydeveloperjourney.com>
2023-05-08 13:27:43 -0500

    wip: serves index.html

--- server.js
+++ server.js
@@ -11,7 +11,23 @@ const app = express();
 require('dotenv').config();
 
 // mount middleware
+app.use(express.json()); // creates req.body
+app.use(morgan('dev'));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// use middleware to help express discover the favicon file
+app.use(express.static(path.join(__dirname, 'build')));
+// use middleware to help express discover static assets
 
 // mount routes
+// API routes go here
+
+// "catch all route" - used to always serve index.html
+app.get('/*',(req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // tell the app to listen
+const port = process.env.PORT || 3001;
+app.listen(port, function() {
+    console.log(`Express app is running on port:${port}`);
+});

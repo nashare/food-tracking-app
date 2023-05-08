928e6cf3dc125faa053d5a8360c0030427546900
trizzlenova <tdswayzee@gmail.com>
2023-05-08 15:17:50 -0700

    navbar

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,20 +2,25 @@ import "./App.css";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
+import NavBar from "../../components/NavBar/NavBar";
+
 import AuthPage from "../AuthPage/AuthPage";
 import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
   return (
     <main className="App">
       {user ? (
-        <Routes>
-          {/* Route components here */}
-          <Route path="/orders/new" element={<NewOrderPage />} />
-          <Route path="/orders" element={<OrderHistoryPage />} />
-        </Routes>
+        <>
+          <NavBar />
+          <Routes>
+            {/* Route components here */}
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
       ) : (
         <AuthPage />
       )}

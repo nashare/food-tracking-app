fa4ca0fd1e83048acd92e62e799a93e7e8601ffe
trizzlenova <tdswayzee@gmail.com>
2023-05-08 14:49:12 -0700

    routes, page components

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <main className="App">
-      App
+      {user ? (
+        <Routes>
+          {/* Route components here */}
+          <Route path="/orders/new" element={<NewOrderPage />} />
+        </Routes>
+      ) : (
+        <AuthPage />
+      )}
     </main>
   );
 }

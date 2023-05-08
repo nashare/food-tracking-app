dd319c12bedb84fb33f7d49eb7a5e60c586d74fc
Daniel JS <daniel@mydeveloperjourney.com>
2023-05-08 12:59:39 -0500

    wip: adds latest updates

---
+++ server.js
@@ -0,0 +1,17 @@
+// require dependencies
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const morgan = require('morgan');
+
+// initialize the express app
+const app = express();
+
+// configure settings
+require('dotenv').config();
+
+// mount middleware
+
+// mount routes
+
+// tell the app to listen

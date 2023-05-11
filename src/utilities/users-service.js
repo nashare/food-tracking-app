d078bb5c2e2016ee54323a02efa38f59d7dbfa89
Daniel JS <daniel@mydeveloperjourney.com>
2023-05-11 12:24:03 -0500

    wip: persists token in localstorage

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -2,6 +2,6 @@ import * as userAPI from './users-api';
 
 export async function signUp(userData) {
     const token = await userAPI.signUp(userData);
-    // TODO: more user service related tasks to handled here later
+    localStorage.setItem('token', token);
     return token;
-}
+};

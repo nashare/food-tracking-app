33e0c5d6625b565f2035e2f60b2de563b11e2de6
trizzlenova <tdswayzee@gmail.com>
2023-05-11 15:43:36 -0700

    refactor/order history

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,43 +1,46 @@
-import * as userAPI from './users-api';
+import * as userAPI from "./users-api";
 
 export async function signUp(userData) {
-    const token = await userAPI.signUp(userData);
-    localStorage.setItem('token', token);
-    return getUser();
-};
-
-export function getToken() {
-    // attempt get the tokem from localstorage
-    const token = localStorage.getItem('token');
-    if(!token) return null;
-    // if a token is retrieved
-        // decode the payload from the token so we can check if it's still valid
-    const payload = JSON.parse(atob(token.split('.')[1]));
-        // (check if it's expired or not)
-    if(payload.exp < Date.now() / 1000) {
-        // remove the token from localstorage
-        localStorage.removeItem('token')
-        return null;
-    }
-    // if still valid, we return the retrieved token
-    return token;
-};
-
-export function getUser() {
-    const token = getToken();
-    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
-};
-
-export async function login(userData) {
-    const token = await userAPI.login(userData);
-    localStorage.setItem('token', token);
-    return getUser();
-};
-
-export function logOut() {
-    localStorage.removeItem('token');
+  const token = await userAPI.signUp(userData);
+  localStorage.setItem("token", token);
+  return getUser();
 }
 
+export function getToken() {
+  // attempt get the tokem from localstorage
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+  // if a token is retrieved
+  // decode the payload from the token so we can check if it's still valid
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  // (check if it's expired or not)
+  if (payload.exp < Date.now() / 1000) {
+    // remove the token from localstorage
+    localStorage.removeItem("token");
+    return null;
+  }
+  // if still valid, we return the retrieved token
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split(".")[1])).user : null;
+}
+
+export async function login(userData) {
+  const token = await userAPI.login(userData);
+  localStorage.setItem("token", token);
+  return getUser();
+}
+
+export function logOut() {
+  localStorage.removeItem("token");
+}
+
+export function checkToken() {
+  alert("clicked!");
+}
 
 /*
     {
